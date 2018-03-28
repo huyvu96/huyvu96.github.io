a857cd97c9ea55387096e7a3bd621ed5fc4769dd
huyvu96 <37835217+huyvu96@users.noreply.github.com>
2018-03-28 20:34:35 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,27 @@ const socket = io('https://callvideobyvu96.herokuapp.com/')
 
 $("#div-chat").hide();
 
+let customConfig;
+
+$.ajax({
+  url: "https://service.xirsys.com/ice",
+  data: {
+    ident: "huyvu0505",
+    secret: "9a7cf394-328a-11e8-aebf-56e9e468e26d",
+    domain: "huyvu96.github.io",
+    application: "default",
+    room: "default",
+    secure: 1
+  },
+  success: function (data, status) {
+    // data.d is where the iceServers object lives
+    customConfig = data.d;
+    console.log(customConfig);
+  },
+  async: false
+});
+
+
 socket.on('DANH-SACH-ONLINE', arr =>{
     $("#div-chat").show();
     $("#div-dang-ky").hide();
@@ -31,7 +52,12 @@ function playStream(idVideoTag, stream){
 }
 //openStream().then(stream => playStream('localStream', stream));
 
- const peer = new Peer({key: 'peerjs', host: 'myvideocall96.herokuapp.com', secure: true, port: 443});
+
+ const peer = new Peer({key: 'peerjs',
+  host: 'myvideocall96.herokuapp.com',
+   secure: true,
+    port: 443,
+    config: customConfig});
 //const peer = new Peer({key: '9evme5fuka0b2o6r'});
 peer.on('open', id => {
     $('#my-peer').append(id);
